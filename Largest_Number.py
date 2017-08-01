bf3dbbc67487ce02b9abfc3d711e982881bacea1
Mukesh <mukeshkamlesh.kumar@gmail.com>
2017-08-01 23:29:45 +0530

    Find the Largest Number

---
+++ Largest_Number.py
@@ -0,0 +1,12 @@
+print("Enter the Value :")
+
+a=raw_input()
+b=raw_input()
+c=raw_input()
+
+if(a>b):
+    print('Largest no is :',a)
+elif(b>c):
+    print('Largest no is :',b)
+else:
+    print('Largest no is :',c)
