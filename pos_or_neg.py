478311da1e6f567e0859b4673e74929c6e845ed5
Mukesh <mukeshkamlesh.kumar@gmail.com>
2017-08-01 20:27:03 +0530

    Update pos_or_neg.py

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -2,7 +2,7 @@ print("Enter the number to check positive or negative :")
 a=int(raw_input())
 if (a==0):
     print("Zero")
-elif(a%2==0):
+elif(a>0):
     print("positive")
 else:
     print ("negative")
